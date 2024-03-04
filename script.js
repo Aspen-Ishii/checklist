2e0001ffc286b1f418274785dd624720fd2b4a04
Aspen Ishii <ishii.aspen@gmail.com>
2024-03-04 14:36:53 -0700

    Append new elements individually instead of directly to innerHTML of task list

--- script.js
+++ script.js
@@ -1,24 +1,31 @@
-document.getElementById('addTaskBtn').onclick = function(){
-    console.log('working');
+    
+    document.getElementById('addTaskBtn').onclick = function(){
+        console.log('working');
+    
     if(document.querySelector('#newTask input').value.length == 0){
         alert("Error: Must enter text")
     } else {
-        document.querySelector('#taskList').innerHTML += `
-            <div class="task">
-                <span id="taskList">
-                    ${document.querySelector('#newTask input').value}
-                </span>
-                <button class="delete">
-                    <i class="far fa-trash-alt"></i>
-                </button>
-            </div>
-        `;
+        // Create a new list item
+        let listItem = document.createElement('li');
+        listItem.textContent = document.querySelector('#newTask input').value;
 
-        var current_task = document.querySelectorAll(".delete");
-        for(var i=0; i<current_task.length; i++){
-            current_task[i].onclick = function(){
-                this.parentNode.remove();
-            }
-        }
-    }
-}
+        // Create a delete button for the list item
+        let deleteBTN = document.createElement('button');
+        deleteBTN.innerHTML = '<i class="fa-regular fa-circle-check"></i>';
+        deleteBTN.classList.add('delete');
+
+        // Append the delete button to the list item
+        listItem.appendChild(deleteBTN);
+
+        // Append the list item to the task list
+        document.querySelector('#taskList').appendChild(listItem);
+
+        // Clear the input field after adding the task
+        document.querySelector('#newTask input').value = '';
+
+        // Attach event listener to the delete button
+        deleteBTN.onclick = function(){
+            this.parentNode.remove();
+        };
+    };
+};
